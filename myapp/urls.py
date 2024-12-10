e2d73a184444254da280eae9bd950fa2ca5200a6
Oishiii <94029123+Neeeeeeooooo@users.noreply.github.com>
2024-12-10 13:30:28 +0800

    django OTP with firebase

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('verify_otp/', views.verify_otp_view, name='verify_otp'),
     path('user/', views.user_view, name='user'),
-    path('admin/', views.admin_view, name='admin'),
+    path('teacher/', views.admin_view, name='teacher'),
     path('logout/', views.logout_view, name='logout'),
 ]
