484e59ff206e65a51229d5c0d8d7b30c9088a3d0
Oishiii <94029123+Neeeeeeooooo@users.noreply.github.com>
2024-12-10 13:12:08 +0800

    django OTP with firebase

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 urlpatterns = [
     path('', lambda request: redirect('login')),
-    path('login/', views.login_view, name='login'),
-    path('register/', views.register_view, name='register'),
-    path('verify_otp/', views.verify_otp_view, name='verify_otp'),
-    path('user/', views.user_view, name='user'),
-    path('admin/', views.admin_view, name='admin'),
-    path('logout/', views.logout_view, name='logout'),
+    path('myapp/login/', views.login_view, name='login'), 
+    path('myapp/register/', views.register_view, name='register'),
+    path('myapp/verify_otp/', views.verify_otp_view, name='verify_otp'),
+    path('myapp/user/', views.user_view, name='user'),
+    path('myapp/admin/', views.admin_view, name='admin'),
+    path('myapp/logout/', views.logout_view, name='logout'),
 ]
